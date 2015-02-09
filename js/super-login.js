69350b39f1d06b31107c49bb989a34d7e8c7eb13
3CWebDev <shawn@ccctampabay.com>
2015-02-09 10:25:55 -0500

    Issue #2422617 by xbzzr: Fixed module description.

--- js/super-login.js
+++ js/super-login.js
@@ -3,7 +3,8 @@
 Drupal.behaviors.moduleFilterDynamicPosition = {
   attach: function(context) {
     var $window = $(window);
-
+    
+    $(".messages").prependTo("#user-login");
     $('#edit-pass').keypress(function(e) {
       var s = String.fromCharCode( e.which );
       if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
